440bce58e1c2b543462b0c76a15e2a23abea2ef0
OBFGeneric <125281761+OBFGeneric@users.noreply.github.com>
2024-09-28 02:35:23 +0800

    Update resnet50.py

--- pyunet/lib/resnet50.py
+++ pyunet/lib/resnet50.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 import torchvision.models as models
 
 class ResNetSegmentation(nn.Module):
-    def __init__(self, ch_in=3,ch_out=1):
+    def __init__(self, in_channels):
         super(ResNetSegmentation, self).__init__()
 
         # Load the pre-trained ResNet50 model
         self.backbone = models.resnet50(weights=None)
 
         # Replace the first layer with a convolutional layer with the desired input channels
-        self.backbone.conv1 = nn.Conv2d(ch_in, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.backbone.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
 
         # Remove the fully connected layer and average pooling layer
         self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])
